301a81eee6acc2ea77df48356ee265b5bae5db7a
shubham jaiswal <115568349+shubhamjaiswal1998@users.noreply.github.com>
2025-02-02 13:25:31 +0530

    Add files via upload

---
+++ InjectedApplication.java
@@ -0,0 +1,13 @@
+package Injection1.injected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectedApplication.class, args);
+	}
+
+}
