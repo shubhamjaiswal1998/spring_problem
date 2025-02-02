0d92c64a901c0bda10ff8e4fcc85ab054681ce65
shubham jaiswal <115568349+shubhamjaiswal1998@users.noreply.github.com>
2025-02-02 13:43:15 +0530

    Add files via upload

--- InjectedApplication.java
+++ InjectedApplication.java
@@ -1,13 +1,50 @@
 package Injection1.injected;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+interface  c1{
+	public  void f2();
+}
+@Component
+class c2 implements c1{
+	@Override
+	public  void f2() {
+		System.out.println("f2");
+	}
+}
+@Component
+class c3{
+	/*@Autowired field injection
+	c2 ob2;*/
 
+	/*c2 ob2;
+	@Autowired      constructer injection
+	public c3(c2 ob2) {
+		this.ob2 = ob2;
+	}*/
+
+	c2 ob2;
+	@Autowired  //setter injection
+	public void setOb2(c2 ob2) {
+		this.ob2 = ob2;
+	}
+
+	public  void f3() {
+		System.out.println("f3");
+		ob2.f2();
+	}
+}
 @SpringBootApplication
 public class InjectedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InjectedApplication.class, args);
+
+	ApplicationContext ctx=SpringApplication.run(InjectedApplication.class, args);
+	c3 ob3=ctx.getBean(c3.class);
+	ob3.f3();
+
 	}
 
 }
